704aaf368fd5255583d8b5c51294bd8299e68244
Akbar Yahya <11765082+akbaryahya@users.noreply.github.com>
2022-05-27 02:36:00 +0800

    add pages error 404 and login for func later

---
+++ src/pages/404.js
@@ -0,0 +1,18 @@
+// 404.js
+import Link from 'next/link'
+
+export default function FourOhFour() {
+    return <>
+
+        <div class="hero min-h-screen bg-base-200">
+            <div class="hero-content text-center">
+                <div class="max-w-md">
+                    <h1 class="text-5xl font-bold">404</h1>
+                    <p class="py-6">If you are playing and get this 404 error, please update our domain to keep playing. click back for how to connect again</p>
+                    <Link href="/"><button class="btn btn-primary">Back to Home</button></Link>
+                </div>
+            </div>
+        </div>
+
+    </>
+}
---
+++ src/pages/login.js
@@ -0,0 +1,36 @@
+import Link from 'next/link'
+
+export default function Login() {
+    return <>
+        <div class="hero min-h-screen bg-base-200">
+            <div class="hero-content flex-col lg:flex-row-reverse">
+                <div class="text-center lg:text-left">
+                    <h1 class="text-5xl font-bold">Server Yuuki</h1>
+                    <p class="py-6">To start playing game, please login with your private server account. if you need help please visit our discord.</p>
+                </div>
+                <div class="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+                    <div class="card-body">
+                        <div class="form-control">
+                            <label class="label">
+                                <span class="label-text">Username</span>
+                            </label>
+                            <input type="text" placeholder="username" class="input input-bordered" />
+                        </div>
+                        <div class="form-control">
+                            <label class="label">
+                                <span class="label-text">Password</span>
+                            </label>
+                            <input type="text" placeholder="password" class="input input-bordered" />
+                            <label class="label">
+                                <a href="#" class="label-text-alt link link-hover">Forgot password?</a>
+                            </label>
+                        </div>
+                        <div class="form-control mt-6">
+                            <button class="btn btn-primary">Login</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </>
+}
