3cc0aba609990b7e649d4d78ce439c7a1e04a10c
Akbar Yahya <11765082+akbaryahya@users.noreply.github.com>
2022-08-14 16:18:41 +0800

    add total Traveler

--- src/components/online.js
+++ src/components/online.js
@@ -1,50 +1,52 @@
-import useSWR from 'swr';
+import useSWR from "swr";
 
 const fetcher = (url) => fetch(url).then((res) => res.json());
 
 export default function App({ server = "sg" }) {
+    const { data, error } = useSWR(`https://${server}.game.yuuki.me/status/server`, fetcher);
 
-  const { data, error } = useSWR(
-    `https://${server}.game.yuuki.me/status/server`,
-    fetcher
-  );
-
-  if (server == "sg") {
-    server = "Singapore"
-  }
-  if (server == "eu") {
-    server = "Dusseldorf"
-  }
-  if (server == "tes") {
-    server = "Beta Testing"
-  }
-
-  var online = "Offline";
-  var version = "Unknown";
-  var ram = "Unknown";
-  if (data) {
-    if (data.status) {
-      if (data.status.playerCount) {
-        online = data.status.playerCount + " Online";
-      }
-      if (data.status.DockerGC) {
-        version = data.status.DockerGC;
-      }
-      if (data.status.MemoryCurrently) {
-        ram = data.status.MemoryCurrently + " MB";
-      }
+    if (server == "sg") {
+        server = "Singapore";
     }
-  }
+    if (server == "eu") {
+        server = "Dusseldorf";
+    }
+    if (server == "tes") {
+        server = "Beta Testing";
+    }
 
-  return (
-    <div class="stat">
-      <div class="stat-title">{server}</div>
-      <div class="stat-value">{online}</div>
-      <div class="stat-desc">
-        <div className="new-line">Version: {version}</div>
-        <div className="new-line">RAM: {ram}</div>
-      </div>
-    </div>
+    var online = "Offline";
+    var version = "Unknown";
+    var traveler = "Unknown";
+    var ram = "Unknown";
+    
+    if (data) {
+        if (data.status) {
+            if (data.status.playerCount) {
+                online = data.status.playerCount + " Online";
+            }
+            if (data.status.DockerGC) {
+                version = data.status.DockerGC;
+            }
+            if (data.status.TotalAccount) {
+                traveler = new Intl.NumberFormat("en-US").format(data.status.TotalAccount);
+            }
+            if (data.status.MemoryCurrently) {
+                ram = data.status.MemoryCurrently + " MB";
+            }
+        }
+    }
 
-  );
+    return (
+        <div class="stat">
+            <div class="stat-title">{server}</div>
+            <div class="stat-value">{online}</div>
+            <div class="stat-title">Traveler</div>
+            <div class="stat-value">{traveler}</div>
+            <div class="stat-desc">
+                <div className="new-line">Version: {version}</div>
+                <div className="new-line">RAM: {ram}</div>
+            </div>
+        </div>
+    );
 }
