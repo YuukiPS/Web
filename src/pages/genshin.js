9c4cd4396979e20af264e56383e58180bd4deccd
Akbar Yahya <11765082+akbaryahya@users.noreply.github.com>
2022-08-15 16:52:47 +0800

    add link download metadata

--- src/pages/genshin.js
+++ src/pages/genshin.js
@@ -5,22 +5,21 @@ import useSWR from "swr";
 const fetcher = (...args) => fetch(...args).then((res) => res.json());
 
 function getLatest(region = "os") {
-    const { data, error } = useSWR(`api/latest/${region}`, fetcher);
-    return {
-        info: data,
-        isLoading: !error && !data,
-        isError: error,
-    };
+    return useSWR(`api/latest/${region}`, fetcher).data;
 }
 
 export default function Genshin() {
-    const raw_os = getLatest("os").info;
-    const raw_cn = getLatest("cn").info;
+    const raw_os = getLatest("os");
+    const raw_cn = getLatest("cn");
+
+    console.log(raw_os);
 
     let version = "Unknown";
 
     let DL_OS = "#";
     let DL_CN = "#";
+    let Decompressed_OS = "#";
+    let Decompressed_CN = "#";
 
     // for OS
     if (raw_os) {
@@ -35,6 +34,10 @@ export default function Genshin() {
 
                     if (latest.path) {
                         DL_OS = latest.path;
+                    }
+
+                    if (latest.decompressed_path) {
+                        Decompressed_OS = latest.decompressed_path;
                     }
                 }
             }
@@ -51,22 +54,40 @@ export default function Genshin() {
                     if (latest.path) {
                         DL_CN = latest.path;
                     }
+
+                    if (latest.decompressed_path) {
+                        Decompressed_CN = latest.decompressed_path;
+                    }
                 }
             }
         }
     }
 
+    let Server_PC = "https://file.yuuki.me/0:/Project/Grasscutter/Game%20Data/PC/";
+    let Metadata_OS_PC_MOD = Server_PC + version + "/Release/Global/global-metadata.dat";
+    let Metadata_CN_PC_MOD = Server_PC + version + "/Release/Chinese/global-metadata.dat";
+    let DL_OS_Backup = Server_PC + version + "/Release/Global/" + DL_OS.substring(DL_OS.lastIndexOf("/") + 1);
+    let DL_CN_Backup = Server_PC + version + "/Release/Chinese/" + DL_CN.substring(DL_CN.lastIndexOf("/") + 1);
+
+    let Metadata_OS_PC_ORI = Decompressed_OS + "/GenshinImpact_Data/Native/Data/Metadata/global-metadata.dat";
+    let Metadata_CN_PC_ORI = Decompressed_CN + "/GenshinImpact_Data/Native/Data/Metadata/global-metadata.dat";
+
     return (
         <>
             <Layout genshin>
+
                 <Head>
                     <title>
                         Download Genshin Impact {version} - {siteTitle}
                     </title>
                 </Head>
+                
                 <h1 class="title">Genshin Impact</h1>
                 <p class="description">Currently version {version}, download link below.</p>
+
                 <div class="grid">
+
+                    <h3 class="text-center py-3">Full Version (Original)</h3>
                     <div class="flex justify-center gap-2 py-3">
                         <a href={`${DL_OS}`} class="btn btn-wide">
                             Global
@@ -76,8 +97,41 @@ export default function Genshin() {
                             China
                         </a>
                     </div>
+
+                    <h3 class="text-center py-3">Full Version (Alternative)</h3>
+                    <div class="flex justify-center gap-2 py-3">
+                        <a href={`${DL_OS_Backup}`} class="btn btn-wide">
+                            Global
+                        </a>
+
+                        <a href={`${DL_CN_Backup}`} class="btn btn-wide">
+                            China
+                        </a>
+                    </div>
+
+                    <h3 class="text-center py-3">Metadata (Patch)</h3>
+                    <div class="flex justify-center gap-2 py-3">
+                        <a href={`${Metadata_OS_PC_MOD}`} class="btn btn-wide">
+                            Global
+                        </a>
+
+                        <a href={`${Metadata_CN_PC_MOD}`} class="btn btn-wide">
+                            China
+                        </a>
+                    </div>
+
+                    <h3 class="text-center py-3">Metadata (Original)</h3>
+                    <div class="flex justify-center gap-2 py-3">
+                        <a href={`${Metadata_OS_PC_ORI}`} class="btn btn-wide">
+                            Global
+                        </a>
+
+                        <a href={`${Metadata_CN_PC_ORI}`} class="btn btn-wide">
+                            China
+                        </a>
+                    </div>
+
                 </div>
-                TODO
             </Layout>
         </>
     );
