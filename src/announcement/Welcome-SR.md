665822366e5aa96a5a1d69d37762095527cdae21
Akbar Yahya <11765082+akbaryahya@users.noreply.github.com>
2022-08-22 03:54:28 +0800

    tes event info

--- src/announcement/Welcome-SR.md
+++ src/announcement/Welcome-SR.md
@@ -5,6 +5,7 @@ banner: "https://gitlab.com/yukiz/grasscutter-data/-/raw/main/data/hk4e/announce
 lang: "en-US"
 category: "Star Rail"
 date: "2022-07-20"
+start_time
 ---
 
 For more info, please visit discord.yuuki.me
--- src/pages/api/genshin/announcement/info.js
+++ src/pages/api/genshin/announcement/info.js
@@ -3,7 +3,7 @@ import { getAnnouncementData } from "../../../../lib/announcement";
 export default async function handler(req, res) {
     const allPostsData = await getAnnouncementData();
     res.status(200).json({
-        t: "{{SYSTEM_TIME}}", //TODO: Use Time Server?
+        t: Date.now(),
         list: allPostsData,
         total: allPostsData.length,
     });
