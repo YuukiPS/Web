ce5f13b4eddf9b450adb146723a3d9fc4af81810
Akbar Yahya <11765082+akbaryahya@users.noreply.github.com>
2022-09-01 03:47:31 +0800

    better info footer

--- src/components/layout.js
+++ src/components/layout.js
@@ -28,11 +28,19 @@ export default function Layout({ children, home }) {
             <footer class="footer footer-center p-4 bg-base-300 text-base-content">
                 <div>
                     <p>
-                        <a href="https://discord.gg/tRYMG7Nm2D" target="_blank" rel="sponsored">
+                        <a class="float-left mr-2" href="https://discord.gg/tRYMG7Nm2D" target="_blank" rel="sponsored">
                             <img
-                                src="https://discordapp.com/api/guilds/964119462188040202/widget.png?style=banner2" alt="DockerGS"
+                                src="https://discordapp.com/api/guilds/964119462188040202/widget.png?style=shield"
+                                alt="DockerGS"
                             ></img>
                         </a>
+
+                        <img class="float-left mr-2" src="https://hits.seeyoufarm.com/api/count/incr/badge.svg?url=https%3A%2F%2Fps.yuuki.me&count_bg=%2379C83D&title_bg=%23555555&icon=&icon_color=%23E7E7E7&title=hits&edge_flat=true"></img>
+
+                        <a href="https://stats.uptimerobot.com/L8Nx6FL4V" target="_blank" rel="sponsored">
+                            <img class="float-left" alt="Uptime Robot ratio (30 days)" src="https://img.shields.io/uptimerobot/ratio/m792377626-415dee0a93ca530b901863a7?label=Server"></img>
+                        </a>
+
                     </p>
                     <p>
                         <a href="https://yuuki.me/" target="_blank" rel="sponsored">
@@ -40,10 +48,7 @@ export default function Layout({ children, home }) {
                         </a>
                     </p>
                     <p>
-                        Powered by Vercel |{" "}
-                        <a href="https://stats.uptimerobot.com/L8Nx6FL4V" rel="nofollow" target="_blank">
-                            Status Server
-                        </a>
+                        Powered by <a href="https://vercel.com/" target="_blank" rel="sponsored">Vercel</a>
                     </p>
                 </div>
             </footer>
