100668461932c4de91ce9b656ac09467dfe938a6
Akbar Yahya <11765082+akbaryahya@users.noreply.github.com>
2022-10-10 22:09:24 +0800

    add 3.1.53

---
+++ pages/api/genshin/download/archive/3.1.53-pre/cn.js
@@ -0,0 +1,4 @@
+export default function handler(req, res) {
+    // MISS, Requires a key from beta account, if you have one please contact me (me@yuuki.me) to help backup/mirror files from our server.
+    res.status(200).json();
+}
