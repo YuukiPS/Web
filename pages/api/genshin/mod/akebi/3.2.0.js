c40953e9531bd04a97d2f8be737f2277361f39ea
Akbar Yahya <akbaryahya_ym@yahoo>
2022-11-18 10:25:45 +0800

    ops

--- pages/api/genshin/mod/akebi/3.2.0.js
+++ pages/api/genshin/mod/akebi/3.2.0.js
@@ -1,7 +1,8 @@
 export default function handler(req, res) {
     res.status(200).json({
         description: "todo",
-        version: {
+        version: "1.1.1",
+        package: {
             os: {
                 url: "https://minas.crepe.moe/f/0e7707ce726b48828231/?dl=1",
                 md6: "BC15DD51D7DBDC94E56D1BBB02ED3009",
