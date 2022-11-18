e2bee0c4db5838b8996081e0bf3f1e5eff660c96
Akbar Yahya <akbaryahya_ym@yahoo>
2022-11-18 10:04:09 +0800

    add akebi

---
+++ pages/api/genshin/mod/akebi/3.2.0.js
@@ -0,0 +1,15 @@
+export default function handler(req, res) {
+    res.status(200).json({
+        description: "todo",
+        version: {
+            os: {
+                url: "https://minas.crepe.moe/f/0e7707ce726b48828231/?dl=1",
+                md6: "BC15DD51D7DBDC94E56D1BBB02ED3009",
+            },
+            cn: {
+                url: "https://minas.crepe.moe/f/9ec7c8c4ee6d4d86b094/?dl=1",
+                md6: "913D0BC93443C275F660C3DB7904D5FC",
+            },
+        },
+    });
+}
