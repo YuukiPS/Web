b91f79b7fdddd99b2060bc800c0f9c898fc5b7e7
Akbar Yahya <akbaryahya_ym@yahoo>
2022-12-09 23:31:19 +0800

    ops lol

--- pages/api/genshin/mod/akebi/3.2.0.js
+++ pages/api/genshin/mod/akebi/3.2.0.js
@@ -5,11 +5,11 @@ export default function handler(req, res) {
         package: {
             os: {
                 url: "https://minas.crepe.moe/f/0e7707ce726b48828231/?dl=1",
-                md6: "BC15DD51D7DBDC94E56D1BBB02ED3009",
+                md5: "BC15DD51D7DBDC94E56D1BBB02ED3009",
             },
             cn: {
                 url: "https://minas.crepe.moe/f/9ec7c8c4ee6d4d86b094/?dl=1",
-                md6: "913D0BC93443C275F660C3DB7904D5FC",
+                md5: "913D0BC93443C275F660C3DB7904D5FC",
             },
         },
     });
