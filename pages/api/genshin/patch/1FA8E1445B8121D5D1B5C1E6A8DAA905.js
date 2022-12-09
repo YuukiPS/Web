b9ac0a5bc96e6b41b35c2c7ae2297c8e85e8abf5
Akbar Yahya <akbaryahya_ym@yahoo>
2022-12-09 20:19:42 +0800

    Update md5 (os) (3.3)

---
+++ pages/api/genshin/patch/1FA8E1445B8121D5D1B5C1E6A8DAA905.js
@@ -0,0 +1,35 @@
+export default function handler(req, res) {
+    res.status(200).json({
+        version: "3.3.0",
+        channel: "OS",
+        release: "Global",
+        patched: {
+            metode: "RSA",
+            resources: "https://file2.yuuki.me/p/Public/Local_EU/Project/GenshinImpact/Data/PC/3.3.0/Global/",
+            key_patch:  "",
+            // for online patch
+            md5_vaild: {
+                os: "",
+                cn: "",
+            },
+        },
+        original: {
+            resources: "https://autopatchhk.yuanshen.com/client_app/download/pc_zip/20221128113321_cOH1qEM6iRagjJC6/ScatteredFiles/",
+            key_find: {
+                cn: "",
+                os: "",
+            },
+            // for check real file
+            md5_check: {
+                os: {
+                    userassembly: "702671865EBA6BD49DB2DA707CF4B4EF",
+                    metadata: "D7E9C1FFB28DEC883697F5DED6B38A2D",
+                },
+                cn: {
+                    userassembly: "3DCE9FA8A7874C70575D38C4F839C229",
+                    metadata: "3A8C5A55768071BD5D29C6044447AE48",
+                },
+            },
+        },
+    });
+}
