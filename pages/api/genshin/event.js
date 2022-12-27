db51999e6b389e2140429a72048b418ff58befa9
Akbar Yahya <akbaryahya_ym@yahoo>
2022-12-28 07:50:03 +0800

    add event windy

--- pages/api/genshin/event.js
+++ pages/api/genshin/event.js
@@ -10,5 +10,15 @@ export default function handler(req, res) {
             endTime: 1924992000,
             meetCondList: [5072001, 5072002, 5072003, 5072004, 5072005, 5072006, 5072007, 5072008, 5072009, 5072013],
         },
+        /* event windy */
+        {
+            activityId: 5111,
+            activityType: 1504,
+            scheduleId: 5111001,
+            expireCondList: [5111003, 5111010],
+            meetCondList: [5111001, 5111002, 5111004],
+            beginTime: 0,
+            endTime: 1924992000,
+        },
     ]);
 }
