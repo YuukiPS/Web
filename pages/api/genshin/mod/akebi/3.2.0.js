6d3370affb58f2fa602fb93e14ca29f566534a0b
Akbar Yahya <akbaryahya_ym@yahoo>
2022-12-31 00:46:09 +0800

    wait, why not use ssl

--- pages/api/genshin/mod/akebi/3.2.0.js
+++ pages/api/genshin/mod/akebi/3.2.0.js
@@ -4,11 +4,11 @@ export default function handler(req, res) {
         version: "1.1.1",
         package: {
             os: {
-                url: "http://file2.yuuki.me/p/Public/Local_EU/Project/GenshinImpact/Data/PC/3.2.0/Global/Mod/akebi-gc-v1.1.1-global.zip",
+                url: "https://file2.yuuki.me/p/Public/Local_EU/Project/GenshinImpact/Data/PC/3.2.0/Global/Mod/akebi-gc-v1.1.1-global.zip",
                 md5: "BC15DD51D7DBDC94E56D1BBB02ED3009",
             },
             cn: {
-                url: "http://file2.yuuki.me/p/Public/Local_EU/Project/GenshinImpact/Data/PC/3.2.0/Chinese/Mod/akebi-gc-v1.1.1-china.zip",
+                url: "https://file2.yuuki.me/p/Public/Local_EU/Project/GenshinImpact/Data/PC/3.2.0/Chinese/Mod/akebi-gc-v1.1.1-china.zip",
                 md5: "913D0BC93443C275F660C3DB7904D5FC",
             },
         },
--- pages/api/genshin/mod/akebi/3.3.0.js
+++ pages/api/genshin/mod/akebi/3.3.0.js
@@ -4,11 +4,11 @@ export default function handler(req, res) {
         version: "1.2.2",
         package: {
             os: {
-                url: "http://file2.yuuki.me/p/Public/Local_EU/Project/GenshinImpact/Data/PC/3.3.0/Global/Mod/akebi-gc-v1.2.2.zip",
+                url: "https://file2.yuuki.me/p/Public/Local_EU/Project/GenshinImpact/Data/PC/3.3.0/Global/Mod/akebi-gc-v1.2.2.zip",
                 md5: "0C4126CB79303618E0D70D9FA91C8956",
             },
             cn: {
-                url: "http://file2.yuuki.me/p/Public/Local_EU/Project/GenshinImpact/Data/PC/3.3.0/Global/Mod/akebi-gc-v1.2.2.zip",
+                url: "https://file2.yuuki.me/p/Public/Local_EU/Project/GenshinImpact/Data/PC/3.3.0/Global/Mod/akebi-gc-v1.2.2.zip",
                 md5: "0C4126CB79303618E0D70D9FA91C8956",
             },
         },
