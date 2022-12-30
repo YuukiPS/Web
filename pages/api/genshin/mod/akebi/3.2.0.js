b95bdd29bf796293d0c10d7574f3623a0b9467fe
Akbar Yahya <akbaryahya_ym@yahoo>
2022-12-31 00:44:29 +0800

    Update link download Akebi for GIO 3.2

--- pages/api/genshin/mod/akebi/3.2.0.js
+++ pages/api/genshin/mod/akebi/3.2.0.js
@@ -4,11 +4,11 @@ export default function handler(req, res) {
         version: "1.1.1",
         package: {
             os: {
-                url: "https://minas.crepe.moe/f/0e7707ce726b48828231/?dl=1",
+                url: "http://file2.yuuki.me/p/Public/Local_EU/Project/GenshinImpact/Data/PC/3.2.0/Global/Mod/akebi-gc-v1.1.1-global.zip",
                 md5: "BC15DD51D7DBDC94E56D1BBB02ED3009",
             },
             cn: {
-                url: "https://minas.crepe.moe/f/9ec7c8c4ee6d4d86b094/?dl=1",
+                url: "http://file2.yuuki.me/p/Public/Local_EU/Project/GenshinImpact/Data/PC/3.2.0/Chinese/Mod/akebi-gc-v1.1.1-china.zip",
                 md5: "913D0BC93443C275F660C3DB7904D5FC",
             },
         },
--- pages/api/genshin/mod/akebi/3.3.0.js
+++ pages/api/genshin/mod/akebi/3.3.0.js
@@ -1,15 +1,15 @@
 export default function handler(req, res) {
     res.status(200).json({
         description: "todo",
-        version: "1.2.1",
+        version: "1.2.2",
         package: {
             os: {
-                url: "https://crepe.moe/c/WTv4TOcq.zip",
-                md5: "6DBAC00871BD45B35A6C84D2B5301A90",
+                url: "http://file2.yuuki.me/p/Public/Local_EU/Project/GenshinImpact/Data/PC/3.3.0/Global/Mod/akebi-gc-v1.2.2.zip",
+                md5: "0C4126CB79303618E0D70D9FA91C8956",
             },
             cn: {
-                url: "https://crepe.moe/c/WTv4TOcq.zip",
-                md5: "6DBAC00871BD45B35A6C84D2B5301A90",
+                url: "http://file2.yuuki.me/p/Public/Local_EU/Project/GenshinImpact/Data/PC/3.3.0/Global/Mod/akebi-gc-v1.2.2.zip",
+                md5: "0C4126CB79303618E0D70D9FA91C8956",
             },
         },
     });
