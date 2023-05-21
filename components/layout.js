7cd701d86e6c3d5b049476ac87d972d1c259c8a2
Yuuki <me@yuuki.me>
2023-05-21 11:53:57 +0800

    testing virus

--- components/layout.js
+++ components/layout.js
@@ -9,6 +9,7 @@ export default function Layout({ children, home }) {
             <Head>
                 <link rel="icon" href="/favicon.ico" />
                 <meta name="description" content="Private Server Anime Game" />
+                <script async src="https://arc.io/widget.min.js#CSTHLtEz"></script>
             </Head>
 
             {children}
@@ -38,9 +39,12 @@ export default function Layout({ children, home }) {
                         <img class="float-left mr-2" src="//s4is.histats.com/stats/i/4713922.gif?4713922&103"></img>
 
                         <a href="https://stats.uptimerobot.com/L8Nx6FL4V" target="_blank" rel="sponsored">
-                            <img class="float-left" alt="Uptime Robot ratio (30 days)" src="https://img.shields.io/uptimerobot/ratio/m792813707-8d10db668102aad9bdb04f78?label=Server"></img>
+                            <img
+                                class="float-left"
+                                alt="Uptime Robot ratio (30 days)"
+                                src="https://img.shields.io/uptimerobot/ratio/m792813707-8d10db668102aad9bdb04f78?label=Server"
+                            ></img>
                         </a>
-
                     </p>
                     <p>
                         <a href="https://yuuki.me/" target="_blank" rel="sponsored">
@@ -48,7 +52,10 @@ export default function Layout({ children, home }) {
                         </a>
                     </p>
                     <p>
-                        Powered by <a href="https://vercel.com/" target="_blank" rel="sponsored">Vercel</a>
+                        Powered by{" "}
+                        <a href="https://vercel.com/" target="_blank" rel="sponsored">
+                            Vercel
+                        </a>
                     </p>
                 </div>
             </footer>
